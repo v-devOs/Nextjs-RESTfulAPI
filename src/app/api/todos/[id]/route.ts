3233f7f7053a76ef3468513797d172dc9f464785
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-25 22:56:49 -0600

    Validaciones en la actualización

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma'
+import { Todo } from '@prisma/client'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
 
 interface Segments {
   params: {
@@ -7,35 +9,41 @@ interface Segments {
   }
 }
 
-export async function GET(request: Request, { params }: Segments) { 
-
-  const todo = await prisma.todo.findUnique({
-    where: {
-      id: params.id
-    }
-  })
-
-  return !!todo
-    ? NextResponse.json( todo )
-    : NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 });
+const getTodo = async ( id: string ):Promise<Todo | null> => {
+  const todo = await prisma.todo.findUnique({ where: { id: id } })
+  return todo;
 }
+
+export async function GET(request: Request, { params }: Segments) { 
+  const todo = await getTodo( params.id )
+
+  return !todo
+    ? NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 })
+    : NextResponse.json(todo)
+}
+
+const putSchema = yup.object({
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
+})
 
 export async function PUT(request: Request, { params }: Segments) { 
 
-  const todo = await prisma.todo.findUnique({
-    where: {
-      id: params.id
-    }
-  })
+  const todo = await getTodo( params.id );
 
-  const body = await request.json()
+  if( !todo ) 
+    return NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 });
 
-  const updatedTodo = await prisma.todo.update({
-    where: { id : params.id }, 
-    data: { ...body }
-  })
+  try {
+    const { complete, description } = await putSchema.validate( await request.json());
+  
+    const updatedTodo = await prisma.todo.update({
+      where: { id : params.id }, 
+      data: { complete, description }
+    })
 
-  return !!todo
-    ? NextResponse.json( updatedTodo )
-    : NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 });
+    return NextResponse.json( updatedTodo )
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 } )
+  }
 }
