d60870516c6f670a30809f8c4d48ea631e7be1ce
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-25 22:41:30 -0600

    Actualizar entradas - PUT

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -19,3 +19,23 @@ export async function GET(request: Request, { params }: Segments) {
     ? NextResponse.json( todo )
     : NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 });
 }
+
+export async function PUT(request: Request, { params }: Segments) { 
+
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id: params.id
+    }
+  })
+
+  const body = await request.json()
+
+  const updatedTodo = await prisma.todo.update({
+    where: { id : params.id }, 
+    data: { ...body }
+  })
+
+  return !!todo
+    ? NextResponse.json( updatedTodo )
+    : NextResponse.json({ message: `No existe registro con el id: ${params.id}`}, { status: 404 });
+}
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -46,3 +46,4 @@ export async function POST(request: Request) {
     return NextResponse.json( error, { status: 400 })
   }
 }
+
