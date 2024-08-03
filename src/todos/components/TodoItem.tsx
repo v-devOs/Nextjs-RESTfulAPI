3dd2e594c75bea5fe42dffefe861c2148d37d1a8
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-03 13:43:31 -0600

    Estructura y diseño

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,5 +1,8 @@
 import { Todo } from "@prisma/client"
 
+import styles from './TodoItem.module.css'
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5"
+
 interface Props{
   todo: Todo
   //TODO: Acciones que quiero llamar
@@ -7,6 +10,23 @@ interface Props{
 
 export const TodoItem = ({ todo }: Props) => {
   return (
-    <div>{ todo.description }</div>
+    <div className={ todo.complete ? styles.todoDone : styles.todoPending }>
+      <div className="flex flex-col sm:flex-row justify-start gap-4">
+        <div className={`
+          flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 
+          ${ todo.complete ? 'bg-blue-100': 'bg-red-100'}
+        `}>
+          {
+            todo.complete
+            ? (<IoCheckboxOutline size={30}/>)
+            : (<IoSquareOutline size={30}/>)
+          }
+        </div>
+
+        <div className="text-center sm:text-left ">
+          { todo.description }
+        </div>
+      </div>
+    </div>
   )
 }
