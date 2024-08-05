eb3789810a644620dee1fc9e375bdd93eeb462b9
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-08-05 16:56:32 -0600

    useOptimistic Hook - React

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -5,8 +5,18 @@ import { Todo } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 
+export const sleep = async( seconds: number = 0 ) => {
+  return new Promise( resolve => {
+    setTimeout(() => {
+      resolve( true )
+    }, seconds * 1000);
+  })
+}
+
 
 export const toggleTodo = async( id: string, complete: boolean): Promise<Todo> => {
+
+  await sleep(3);
 
   const todo = await prisma.todo.findFirst({ where: { id }})
 
